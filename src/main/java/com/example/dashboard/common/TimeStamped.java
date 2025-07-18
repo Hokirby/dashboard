fb13cd6c3b9364d01e772ed95c80d7fdc4179489
HoKirby <lakevely27@gmail.com>
2025-07-18 14:28:17 +0900

    fix: createdAt 값이 null 처리 되는 오류

--- src/main/java/com/example/dashboard/common/TimeStamped.java
+++ src/main/java/com/example/dashboard/common/TimeStamped.java
@@ -1,10 +1,7 @@
 package com.example.dashboard.common;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,12 +9,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.sql.Timestamp;
 
 @Getter
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimeStamped {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(updatable = false, nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd HH:mm:SS")
     private Timestamp createdAt;
 }
